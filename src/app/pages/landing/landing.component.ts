86765ecad012b7adf54326c4448500cfba3f6573
albertsparrman <albert.sparrman@gmail.com>
2025-05-21 13:44:04 +0200

    landing page hero fix and backgorund color change

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { CommonModule } from '@angular/common';
 
@@ -9,16 +9,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
-  shine = '200rem'
+export class LandingComponent implements AfterViewInit {
+  /*   shine = '150vw' */
   backgroundOpacity = ''
-  screenHeight: number = 0;
+  screenHeight: number = 0
+  backgroundColor = '#1a75ff4d'
+  color = 'var(--color-light-text)'
 
   @ViewChild('newSection') section: ElementRef | undefined;
   @ViewChild('newSection2') section2: ElementRef | undefined;
 
-  constructor() {
-
+  ngAfterViewInit(): void {
     if (typeof window !== "undefined") {
       this.onWindowScroll()
       this.onResize();
@@ -27,23 +28,33 @@ export class LandingComponent {
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
-    this.shine = 200 - (window.scrollY/3) + 'rem'
+    /*     this.shine = 150 - (window.scrollY/(1 + this.screenHeight/100)) + 'vw'
+        console.log(window.scrollY/(1 + this.screenHeight/100));
+         */
+    if (window.scrollY >= this.section2?.nativeElement.offsetTop) {
+      this.backgroundColor = 'var(--color-light)'
+      this.color = 'var(--color-dark)'
+    }
+    else {
+      this.backgroundColor = '#1a75ff4d'
+      this.color = 'var(--color-light-text)'
+    }
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.screenHeight = window.innerHeight;
     console.log(this.section?.nativeElement.offsetTop);
-    
+
   }
 
-/*   ngAfterViewInit() {
-    
-
-    if (typeof window !== "undefined") {
-      this.onWindowScroll()
+  /*   ngAfterViewInit() {
+      
+  
+      if (typeof window !== "undefined") {
+        this.onWindowScroll()
+      }
     }
-  }
- */
+   */
 
 }
