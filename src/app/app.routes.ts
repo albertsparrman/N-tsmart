f7a8955f1159f0153bbbde72f3c99d394bfc2cbf
albertsparrman <albert.sparrman@gmail.com>
2025-05-21 16:08:57 +0200

    Select courses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
+import { CoursesComponent } from './pages/courses/courses.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent, title: "Nätsmart"},
-    {path: 'snabbkurser', component: ArticlesComponent, title: "Nätsmart - Snabbkurser"},
+    {path: 'snabbkurser', component: CoursesComponent, title: "Nätsmart - Snabbkurser"},
     {path: 'artiklar', component: ArticlesComponent, title: "Nätsmart - Artiklar"},
     {path: 'verktyg', component: ArticlesComponent, title: "Nätsmart - Verktyg"}
 ];
--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NavComponent } from '../../components/nav/nav.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+   @ViewChild('gfgform') form: JSON | undefined;
 
+  checkform() {
+    console.log('hallå');
+    if (this.form != undefined) {
+          console.log(this.form);
+    }    
+  }
 }
