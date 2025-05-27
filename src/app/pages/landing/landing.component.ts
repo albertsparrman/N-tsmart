6656b05a4fae15bf0ea0dbcdbe837058a3610024
albertsparrman <albert.sparrman@gmail.com>
2025-05-27 14:34:34 +0200

    landing fixes

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,36 +2,33 @@ import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@
 import { CommonModule } from '@angular/common';
 import { CourseCardComponent } from "../../components/course-card/course-card.component";
 import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, CourseCardComponent],
+  imports: [CommonModule, CourseCardComponent, RouterLink],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent implements AfterViewInit {
+export class LandingComponent {
   coursesData: any;
   url: string = '../../../assets/courses.json'
   /*   shine = '150vw' */
-  backgroundOpacity = ''
   screenHeight: number = 0
   backgroundColor = '#1a75ff4d'
   color = 'var(--color-light-text)'
+  backgroundColorBouble = 'var(--color-light)'
 
 
   constructor(private http: HttpClient) { }
 
-  @ViewChild('newSection') section: ElementRef | undefined;
   @ViewChild('newSection2') section2: ElementRef | undefined;
 
   ngOnInit() {
     this.http.get(this.url).subscribe(res => {
       this.coursesData = res;
     })
-  }
-
-  ngAfterViewInit(): void {
     if (typeof window !== "undefined") {
       this.onWindowScroll()
       this.onResize();
@@ -43,21 +40,21 @@ export class LandingComponent implements AfterViewInit {
     /*     this.shine = 150 - (window.scrollY/(1 + this.screenHeight/100)) + 'vw'
         console.log(window.scrollY/(1 + this.screenHeight/100));
          */
-    if (window.scrollY >= this.section2?.nativeElement.offsetTop - this.screenHeight /2) {
+    if (window.scrollY >= this.section2?.nativeElement.offsetTop - this.screenHeight / 2) {
       this.backgroundColor = 'var(--color-light)'
       this.color = 'var(--color-dark)'
+      this.backgroundColorBouble = 'var(--color-light-text)'
     }
     else {
       this.backgroundColor = '#1a75ff4d'
       this.color = 'var(--color-light-text)'
+      this.backgroundColorBouble = 'var(--color-light)'
     }
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.screenHeight = window.innerHeight;
-    console.log(this.section?.nativeElement.offsetTop);
-
   }
 
   /*   ngAfterViewInit() {
