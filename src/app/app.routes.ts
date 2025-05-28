952faa8c9c2aa1f7428fbad0a8ab609fd5f10327
albertsparrman <albert.sparrman@gmail.com>
2025-05-28 11:13:33 +0200

    added tools route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { CourseComponent } from './pages/course/course.component';
+import { ToolsComponent } from './pages/tools/tools.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent, title: "Nätsmart"},
     {path: 'snabbkurser', component: CoursesComponent, title: "Nätsmart - Snabbkurser"},
     {path: 'artiklar', component: ArticlesComponent, title: "Nätsmart - Artiklar"},
-    {path: 'verktyg', component: ArticlesComponent, title: "Nätsmart - Verktyg"},
+    {path: 'verktyg', component: ToolsComponent, title: "Nätsmart - Verktyg"},
 
     {path:'snabbkurser/:name/:id', component: CourseComponent, title: "Nätsmart"},
 
