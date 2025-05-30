1f673fa12d79884dcc511c0c5da0c0e3e442380c
albertsparrman <albert.sparrman@gmail.com>
2025-05-30 09:26:10 +0200

    added about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
-import { ArticlesComponent } from './pages/articles/articles.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { CourseComponent } from './pages/course/course.component';
 import { ToolsComponent } from './pages/tools/tools.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent, title: "Nätsmart"},
     {path: 'snabbkurser', component: CoursesComponent, title: "Nätsmart - Snabbkurser"},
-    {path: 'artiklar', component: ArticlesComponent, title: "Nätsmart - Artiklar"},
+    {path: 'om oss', component: AboutUsComponent, title: "Nätsmart - Om oss"},
     {path: 'verktyg', component: ToolsComponent, title: "Nätsmart - Verktyg"},
 
     {path:'snabbkurser/:name/:id', component: CourseComponent, title: "Nätsmart"},
