8ed21098e1e331e2576b02a0e99edac521e0b7aa
albertsparrman <albert.sparrman@gmail.com>
2025-05-31 12:22:34 +0200

    updated app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes,
   withInMemoryScrolling({
     scrollPositionRestoration: 'top'
-  }) ), provideClientHydration()]
+  }) ), provideHttpClient(withFetch()), provideRouter(routes), provideClientHydration()]
 };
