a3604e7d01a473141ec32509a2ac0523f7ed6b21
albertsparrman <albert.sparrman@gmail.com>
2025-06-03 17:19:36 +0200

    footer fix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  constructor(
+    public router: Router
+  ) { }
 }
